2fb7252d574fc8221dbfb690f158953006e0f077
AwedftyhjioL <youkequan@outlook.com>
2023-11-12 05:23:03 +0800

    remove imgui

--- gui/ImguiWinHelper.cpp
+++
@@ -1,8 +0,0 @@
-//
-// Created by Y on 2023/11/8.
-//
-
-#include "ImguiWinHelper.h"
-
-namespace Gui {
-} // Gui
--- main.cpp
+++ main.cpp
@@ -1,14 +1,11 @@
 #include <iostream>
 
-#include "gui/ImguiDemo.h"
 
 
 int main()
 {
     std::cout << "Hello, World!" << std::endl;
     
-    Gui::ImguiDemo imgui_test_window;
-    imgui_test_window.Run();
     
     return 0;
 }
